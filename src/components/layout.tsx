8ed9c3303983f60df59a2481c37ca10b04067ada
Razze <razzeee@gmail.com>
2021-03-13 23:39:11 +0100

    Create carousel component
    Added swiper and scss (we will probably need scss later anyway).
    I choose to use swiper as it should enable us to work flawlessly on
    mobile.
    
    gatsby-config.js was also formatted by prettier.
    
    I've added a test carousel to the layout page

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import Header from './header'
+import React from "react";
+import Carousel from "./carousel";
+import Header from "./header";
 
-function Layout (props) {
-  let frontmatter = {}
+function Layout(props) {
+  let frontmatter = {};
   if (props.pageContext === undefined) {
-    frontmatter['title'] = ''
+    frontmatter["title"] = "";
   } else {
     if (props.pageContext.frontmatter === undefined) {
-      frontmatter['title'] = ''  
+      frontmatter["title"] = "";
     } else {
-      frontmatter = props.pageContext.frontmatter
+      frontmatter = props.pageContext.frontmatter;
     }
   }
   return (
@@ -19,15 +20,25 @@ function Layout (props) {
         <main>
           <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
             <div class="px-4 py-6 sm:px-0">
-              <div name="content" class="prose prose-blue max-w-7xl lg:prose-lg 2xl:prose-2xl">
+              <div
+                name="content"
+                class="prose prose-blue max-w-7xl lg:prose-lg 2xl:prose-2xl"
+              >
                 {props.children}
               </div>
             </div>
           </div>
         </main>
+
+        <Carousel
+          slides={[
+            "https://source.unsplash.com/weekly?mountain",
+            "https://source.unsplash.com/weekly?sea",
+          ]}
+        ></Carousel>
       </div>
     </>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
